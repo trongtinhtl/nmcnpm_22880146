d3999778aa5048f5e6210d20900dcdef7452ce87
PITRITI <trongtinhtl@gmail.com>
2024-06-01 01:26:42 +0700

    chore: edit title

---
+++ ITConferences/wwwroot/js/register.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    const form = $('#registerForm');
+
+    form.on('change', 'input', function () {
+        if (this.value) {
+            $(this).removeClass('is-invalid');
+            $(this).siblings('.invalid-feedback').hide();
+        }
+        else {
+            $(this).addClass('is-invalid');
+            $(this).siblings('.invalid-feedback').show();
+        }
+
+        var elements = form.find('input.is-invalid');
+
+        if (elements.length) {
+            form.find('[type="submit"]').addClass('disabled');
+        }
+        else {
+            form.find('[type="submit"]').removeClass('disabled');
+        }
+    })
+});
